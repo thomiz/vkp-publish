d015b2268b7ea3756893c8e1978d79fdfd6ecd39
thotr <thomas.tveit.rosenlund@ehelse.no>
2017-09-18 14:59:08 +0200

    Mer eksempel utvikling

--- STU3/REST-kall/Rest-kall.bat
+++ STU3/REST-kall/Rest-kall.bat
@@ -3,3 +3,6 @@ På hjemmepc:
 curl.exe http://localhost:8080/baseDstu2/StructureDefinition/Velocityms --upload-file extensions\Velocityms.structuredefinition.xml
 curl.exe http://tgs-fhir-web.testsenter.nhn.no/baseDstu3/Bundle --upload-file examples/eVik-message-bundle-01.json
 curl.exe http://localhost:8080/baseDstu3/Bundle --upload-file examples/eVik-message-bundle-01.json
+
+curl.exe http://localhost:8080/baseDstu3/StructureDefinition/vft-timestamp --upload-file StructureDefinition\vft-timestamp.structuredefinition.xml
+curl.exe http://localhost:8080/baseDstu3/StructureDefinition/test-flag --upload-file StructureDefinition\test-flag.structuredefinition.xml
